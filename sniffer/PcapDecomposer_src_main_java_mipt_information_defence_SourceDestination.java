575f2af6aa03f992010855111461474f08132353
Viacheslav_Tulintsev <vezon-x@mail.ru>
2019-12-10 21:27:46 +0300

    Delete PcapDecomposer_src_main_java_mipt_information_defence_SourceDestination.java

--- sniffer/PcapDecomposer_src_main_java_mipt_information_defence_SourceDestination.java
+++
@@ -1,9 +0,0 @@
-package mipt.information.defence;
-
-public class SourceDestination {
-  public int sourcePort;
-  public String sourceIp;
-  public int destinationPort;
-  public String destinationIp;
-  public byte[] fragmentedData;
-}
